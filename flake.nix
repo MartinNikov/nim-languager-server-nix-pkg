d2cd26d47a3883a7d7f2c812f4a4a159a79e6910
Martin Nikov <mnikov12@gmail.com>
2023-03-16 19:36:11 +0200

    --

--- flake.nix
+++ flake.nix
@@ -1,10 +1,5 @@
 {
-  description = "nix-blockchain-development";
-
-  nixConfig = {
-    extra-substituters = "https://nix-blockchain-development.cachix.org";
-    extra-trusted-public-keys = "nix-blockchain-development.cachix.org-1:Ekei3RuW3Se+P/UIo6Q/oAgor/fVhFuuuX5jR8K/cdg=";
-  };
+  description = "langserver";
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
--- packages/langserver/default.nix
+++ packages/langserver/default.nix
@@ -1,30 +1,13 @@
-{
-  clangStdenv,
-  nodejs,
-  fetchgit,
-  pkgs,
-  lib,
-}:
-clangStdenv.mkDerivation rec {
-  name = "langserver";
-  version = "0.1.0";
-  buildInputs = with pkgs; [
-    llvm
-    curl.dev
-    gmp.dev
-    openssl.dev
-    libusb1.dev
-    bzip2.dev
-    (boost.override
-      {
-        enableShared = false;
-        enabledStatic = true;
-      })
-  ];
-  nativeBuildInputs = with pkgs; [Nim Language Server];
+{ lib, buildNimPackage, fetchFromGitHub, SDL2 }:
 
-  src = fetchgit {
-    url = "https://github.com/nim-lang/langserver";
-    sha256 = "";
+buildNimPackage rec {
+  pname = "langserver";
+  version = "0.1.0";
+  nimBinOnly = true;
+  src = fetchFromGitHub {
+    owner = "nim-lang";
+    repo = "langserver";
+    rev = "dbaff403fa7f785d1b6afeff890dd6f903cb8b49";
+    hash = "sha256-wkD9+6p8Volp0WwRfLej7QkvsCHXL6B7uPHWBaDpMDw=";
   };
 }
