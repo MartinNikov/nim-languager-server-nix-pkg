99dfb3cbdb3ce15c1696dc59d0b3987f76c66271
Martin Nikov <mnikov12@gmail.com>
2023-03-17 21:10:12 +0200

    --

--- overlay.nix
+++ overlay.nix
@@ -1,10 +1,12 @@
 _finalNixpkgs: prevNixpkgs: let
   langserver = prevNixpkgs.callPackage ./packages/langserver/default.nix { inherit faststreams; };
   faststreams = prevNixpkgs.callPackage ./packages/faststreams/default.nix {};
+  asynctools = prevNixpkgs.callPackage ./packages/asynctools/default.nix {};
 
 in {
   metacraft-labs = rec {
     inherit langserver;
     inherit faststreams;
+    inherit asynctools;
   };
 }
---
+++ packages/asynctools/default.nix
@@ -0,0 +1,8 @@
+{ fetchFromGitHub }:
+
+fetchFromGitHub {
+  owner = "cheatfate";
+  repo = "asynctools";
+  rev = "84ced6d002789567f2396c75800ffd6dff2866f7";
+  hash = "sha256-mrO+WeSzCBclqC2UNCY+IIv7Gs8EdTDaTeSgXy3TgNM=";
+}
