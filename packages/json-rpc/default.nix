34126e2cbf73e3c80daae6bbc3f68d49024f1f7a
Martin Nikov <mnikov12@gmail.com>
2023-03-18 02:45:12 +0200

    --

--- packages/json-rpc/default.nix
+++ packages/json-rpc/default.nix
@@ -1,8 +1,8 @@
 { fetchFromGitHub }:
 
 fetchFromGitHub {
-  owner = "status-im";
+  owner = "yyoncho";
   repo = "nim-json-rpc";
-  rev = "af1276443618974a95dd3c83e57a1ecd70df2c5e";
-  hash = "sha256-0juXk2W/78vK9IAmAkO59Ge1Nkg6iZ7pd1JLa0JBp1Y=";
+  rev = "808ba33269341ed500c9f65f0663e0918917e546";
+  hash = "sha256-MnyVBBvQEqU1ys/hG+hEMt+ddHULn8mVfWuasZlcHvU=";
 }
