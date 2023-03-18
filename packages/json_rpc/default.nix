ccff8cdaf86f853ecca0d2265d24c774fda343fd
Martin Nikov <mnikov12@gmail.com>
2023-03-18 03:18:35 +0200

    --

--- packages/json_rpc/default.nix
+++ packages/json_rpc/default.nix
@@ -1,9 +1,16 @@
-{ fetchFromGitHub }:
+#{ fetchFromGitHub }:
 
-fetchFromGitHub {
-  owner = "yyoncho";
-  repo = "nim-json-rpc";
-  branchName = "notif-changes";
-  rev = "808ba33269341ed500c9f65f0663e0918917e546";
-  hash = "sha256-MnyVBBvQEqU1ys/hG+hEMt+ddHULn8mVfWuasZlcHvU=";
+#fetchFromGitHub {
+#  url = "https://github.com/yyoncho/nim-json-rpc/tree/notif-changes";
+#  owner = "yyoncho";
+#  repo = "nim-json-rpc";
+#  rev = "808ba33269341ed500c9f65f0663e0918917e546";
+#  hash = "sha256-MnyVBBvQEqU1ys/hG+hEMt+ddHULn8mVfWuasZlcHvU=";
+#}
+
+{ lib, fetchurl }:
+
+fetchurl {
+  url = "https://github.com/yyoncho/nim-json-rpc/tree/notif-changes";
+  sha256 = "sha256-z7NhLKUOJioLTGKPvZn7Wzvm961FKn44zYfLryFQKoI=";
 }
