4febf85a2b623c12ab033a951d8f8705771a998e
Martin Nikov <mnikov12@gmail.com>
2023-03-18 02:58:42 +0200

    --

--- overlay.nix
+++ overlay.nix
@@ -3,7 +3,7 @@ _finalNixpkgs: prevNixpkgs: let
   faststreams = prevNixpkgs.callPackage ./packages/faststreams/default.nix {};
   asynctools = prevNixpkgs.callPackage ./packages/asynctools/default.nix {};
   stew = prevNixpkgs.callPackage ./packages/stew/default.nix {};
-  json_rpc = prevNixpkgs.callPackage ./packages/json-rpc/default.nix {};
+  json_rpc = prevNixpkgs.callPackage ./packages/json_rpc/default.nix {};
 
 in {
   metacraft-labs = rec {
--- packages/json_rpc/default.nix
+++ packages/json_rpc/default.nix
@@ -3,6 +3,7 @@
 fetchFromGitHub {
   owner = "yyoncho";
   repo = "nim-json-rpc";
+  branchName = "notif-changes";
   rev = "808ba33269341ed500c9f65f0663e0918917e546";
   hash = "sha256-MnyVBBvQEqU1ys/hG+hEMt+ddHULn8mVfWuasZlcHvU=";
 }
